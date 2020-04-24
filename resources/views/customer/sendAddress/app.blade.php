5f6ca868584de99d4b12669dc5ced900a0357c66
Sitthikorn Magoonta <golfzaa2555@gmail.com>
2020-04-25 01:24:31 +0700

    address

--- resources/views/customer/sendAddress/app.blade.php
+++ resources/views/customer/sendAddress/app.blade.php
@@ -9,15 +9,32 @@
 @section('content')
 
 <div class="container" >
-    <div class="single-testimonial item d-flex flex-row" style="margin: 10px;background-color: #f9f9ff">
-        <div class="desc col-md-12">
-            {{-- <p>
-                Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills, the bigger the payoff you.		     
-            </p>
-            <h4>Harriet Maxwell</h4>
-            <p>CEO at Google</p> --}}
+    <div class="item d-flex flex-row" style="margin: 10px;background-color: #f9f9ff;padding-top: 20px;padding-bottom: 20px">
+        <div class="col-md-12">
             <div class="row">
-
+                <div class="col-md-10">
+                    <div class="row">
+                        <div  class="col-md-10" style="word-wrap:break-word;">
+                            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        </div>
+                        <div class="col-md-2 d-flex align-items-center" style="justify-content: space-around;">
+                            <div style="background-color: #00bfa5;color: white;padding: 5px;border-radius: 5px">
+                                ค่าเริ่มต้น
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row">
+                        <div class="col-md-6 genric-btn success  circle arrow">
+                            <span class="lnr lnr-pencil" style="font-size: 16px"></span>
+                        </div>
+                       
+                        <div class="col-md-6 genric-btn danger circle" >
+                            <span class="lnr lnr-trash"  style="font-size: 16px"></span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div> 
