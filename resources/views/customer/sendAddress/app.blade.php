7ecd0517b06889abab93eb58df428861a21a1e0a
JMS Dev <ap.jmsjame@gmail.com>
2020-04-27 22:18:33 +0700

    div container

--- resources/views/customer/sendAddress/app.blade.php
+++ resources/views/customer/sendAddress/app.blade.php
@@ -8,37 +8,42 @@
 
 @section('content')
 
-<div class="container" >
-    <div class="item d-flex flex-row" style="margin: 10px;background-color: #f9f9ff;padding-top: 20px;padding-bottom: 20px">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-10">
+<div class="whole-wrap">
+    <div class="container">
+        <div class="section-top-border">
+            <div class="item d-flex flex-row"
+                style="margin: 10px;background-color: #f9f9ff;padding-top: 20px;padding-bottom: 20px">
+                <div class="col-md-12">
                     <div class="row">
-                        <div  class="col-md-10" style="word-wrap:break-word;">
-                            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        <div class="col-md-10">
+                            <div class="row">
+                                <div class="col-md-10" style="word-wrap:break-word;">
+                                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                                </div>
+                                <div class="col-md-2 d-flex align-items-center" style="justify-content: space-around;">
+                                    <div style="background-color: #00bfa5;color: white;padding: 5px;border-radius: 5px">
+                                        ค่าเริ่มต้น
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-2 d-flex align-items-center" style="justify-content: space-around;">
-                            <div style="background-color: #00bfa5;color: white;padding: 5px;border-radius: 5px">
-                                ค่าเริ่มต้น
+                        <div class="col-md-2">
+                            <div class="row">
+                                <div class="col-md-6 genric-btn success  circle arrow">
+                                    <span class="lnr lnr-pencil" style="font-size: 16px"></span>
+                                </div>
+
+                                <div class="col-md-6 genric-btn danger circle">
+                                    <span class="lnr lnr-trash" style="font-size: 16px"></span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
-                    <div class="row">
-                        <div class="col-md-6 genric-btn success  circle arrow">
-                            <span class="lnr lnr-pencil" style="font-size: 16px"></span>
-                        </div>
-                       
-                        <div class="col-md-6 genric-btn danger circle" >
-                            <span class="lnr lnr-trash"  style="font-size: 16px"></span>
-                        </div>
-                    </div>
-                </div>
             </div>
+
         </div>
-    </div> 
-                                 
+    </div>
 </div>
 @endsection
 
