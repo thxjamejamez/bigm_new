3f2708492aa0e026d4ded6e1fb16177846553844
JMS Dev <ap.jmsjame@gmail.com>
2020-04-28 22:34:45 +0700

    re\\query

--- app/Http/Controllers/Customer/SendAddressController.php
+++ app/Http/Controllers/Customer/SendAddressController.php
@@ -29,9 +29,15 @@ class SendAddressController extends Controller
         }
 
         $list_send_address = \DB::table('cust_send_address')
+            ->leftjoin('l_district', 'cust_send_address.district_id', '=', 'l_district.id')
+            ->leftjoin('l_amphure', 'cust_send_address.amphure_id', '=', 'l_amphure.id')
+            ->leftjoin('l_province', 'cust_send_address.province_id', '=', 'l_province.id')
             ->where('cust_send_address.cust_id', $cust_id->id)
             ->orderBy('cust_send_address.defualt')
+            ->orderByDesc('cust_send_address.id')
             ->get();
+
+        return response()->json($list_send_address);
 
         return view('customer.sendAddress.app', ['banners' => $banners, 'permission_add' => true, 'list_address' => $list_send_address]);
     }
