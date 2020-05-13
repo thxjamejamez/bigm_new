30f29765a39cedf6a70e0088b1bd5f237036f1f9
James Developer <ap.jmsjame@gmail.com>
2020-05-13 14:07:17 +0700

    layout not done

--- public/js/customer/quotation/addtype1.js
+++ public/js/customer/quotation/addtype1.js
@@ -1,6 +1,59 @@
 const APP = new Vue({
     el: '#app',
     data: {
-        message: 2134
+        controls: {
+            // product: [{
+            //     pd_f_id: 1,
+            //     pd_f_img: '/img/defualt_product.jpg',
+            //     pd_f_name: 'name',
+            //     pd_details: [{
+            //             size: '2814*252*300',
+            //             qty: 5
+            //         },
+            //         {
+            //             size: '200*20*300',
+            //             qty: 6
+            //         }
+            //     ]
+            // }, {
+            //     pd_f_id: 2,
+            //     pd_f_img: '/img/defualt_product.jpg',
+            //     pd_f_name: 'name',
+            //     pd_details: [{
+            //             size: '3000*300*200',
+            //             qty: 8
+            //         },
+            //         {
+            //             size: '300*30*200',
+            //             qty: 9
+            //         }
+            //     ]
+            // }]
+
+            product: [{}],
+            modal: [{
+                id: 2,
+                pd_f_name: 'test01',
+                img_path: '/img/defualt_product.jpg'
+
+            }, {
+                id: 1,
+                pd_f_name: 'test02',
+                img_path: '/img/defualt_product.jpg'
+            }]
+        },
+        actions: {
+            modal: false
+        }
+    },
+    methods: {
+        toggleModal() {
+            $('#add-productformat').modal('toggle');
+        },
+
+        addProductFormat() {
+            console.log(1234);
+
+        }
     }
 })
